16c297348736f8fcf2925c1f1a9370b513cd6884
0ksks <1625436933@qq.com>
2023-12-18 10:06:30 +0800

    add choice

---
+++ src/pages/ProblemCard/choice.vue
@@ -0,0 +1,36 @@
+<template>
+  <card>
+    <div class="row">
+      <blockquote v-for="choice in choicess" :key="choice">
+        <p class="blockquote blockquote-primary">
+          {{ choice }}
+        </p>
+      </blockquote>
+    </div>
+  </card>
+</template>
+<script>
+import { Card } from "@/components/index";
+export default {
+  components: {
+    Card,
+  },
+  props: {
+    questionText: {
+      type: String,
+      default: "",
+    },
+    choices: {
+      type: Array,
+      default: () => [],
+    }
+  },
+  data() {
+    return {
+      choicess: [
+          "c11","c12","c13",
+      ],
+    }
+  }
+}
+</script>
